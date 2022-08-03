1b45d8444f2f387ab793beca905287d2ed988039
Brett Peary <brett.peary@gmail.com>
2022-08-03 16:56:02 +0900

    build: add weworkteal custom tailwind color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        weworkteal: {
+          100: "#cfeeeb",
+          200: "#9fded6",
+          300: "#6fcdc2",
+          400: "#3fbdad",
+          500: "#0fac99",
+          600: "#0c8a7a",
+          700: "#09675c",
+          800: "#06453d",
+          900: "#03221f"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
