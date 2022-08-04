79254c3d2c89ab47ba47c7e4059a75b6b241956e
Brett Peary <brett.peary@gmail.com>
2022-08-04 19:59:44 +0900

    feat: add RoomListItem & RoomListFilter

---
+++ src/lib/RoomListFilter.svelte
@@ -0,0 +1,100 @@
+<script></script>
+
+<div class="mt-8 mb-4 md:flex md:items-center md:justify-between">
+	<form
+		action=""
+		class="flex-col md:flex-row flex-1 flex items-center min-w-0 gap-x-4 gap-y-3 flex-wrap"
+	>
+		<fieldset class="text-xl md:text-base font-semibold leading-7 text-gray-900 whitespace-nowrap ">
+			Find a room
+		</fieldset>
+
+		<div class="bg-gray-100 rounded-2xl py-2 px-4 flex flex-row items-center gap-1">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="flex-shrink-0 mr-1.5 h-[32px] w-[32px] text-weworkteal-600"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+				/>
+			</svg>
+			<label for="date" class="text-md font-semibold text-gray-700">Date</label>
+			<button
+				class="px-3 py-1 inline-flex text-sm ml-2 leading-5 font-bold rounded-full bg-weworkteal-500 text-white hover:bg-weworkteal-600"
+			>
+				Today
+			</button>
+			<button class="text-sm mx-4 text-gray-500">Change</button>
+		</div>
+		<div
+			class="flex-col md:flex-row bg-gray-100 rounded-2xl py-2 px-4 flex items-center gap-1  gap-y-2"
+		>
+			<div class="flex items-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="flex-shrink-0 mr-1.5 h-[32px] w-[32px] text-weworkteal-600"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					stroke-width="2"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+					/>
+				</svg>
+				<label for="people-count" class="text-md font-semibold text-gray-700">People</label>
+			</div>
+
+			<div
+				class="flex flex-row rounded-lg bg-weworkteal-100 text-weworkteal-600 leading-5  py-2  divide-x md:ml-2 divide-solid divide-weworkteal-400 "
+			>
+				<div class="people-count">1</div>
+				<div class="people-count">2</div>
+				<div class="people-count selected">3</div>
+				<div class="people-count">4</div>
+				<div class="people-count">5</div>
+			</div>
+
+			<div class="flex items-center">
+				<input
+					type="number"
+					name="custom-people-count"
+					id="custom-people-count"
+					class="w-20 ml-2 pt-[4px] pb-[2px]  px-2 rounded-lg shadow-inner block inner-shadow"
+					placeholder="Other"
+					min="6"
+				/>
+
+				<button class="text-sm mx-4 text-gray-500">Clear</button>
+			</div>
+		</div>
+		<button class="text-sm mx-4 text-gray-500">Reset</button>
+	</form>
+</div>
+
+<style>
+	.people-count {
+		@apply text-center w-10 flex justify-center items-center -mr-[2px];
+	}
+
+	.people-count.selected {
+		@apply bg-weworkteal-500 -my-2 z-10 rounded-xl font-bold text-white;
+	}
+
+	.people-count.selected:hover {
+		@apply bg-weworkteal-600;
+	}
+
+	.people-count:hover {
+		@apply bg-weworkteal-200 -my-2  z-10 rounded-xl font-bold text-white cursor-pointer;
+		border: none !important;
+	}
+</style>
---
+++ src/lib/RoomListItem.svelte
@@ -0,0 +1,99 @@
+<script>
+</script>
+
+<li class="border-solid border-2 border-gray-100 shadow-sm rounded-lg">
+	<a href="#" class="block hover:bg-gray-50 ">
+		
+        <div class="grid-col-1 sm:grid-col-6 grid md:grid-cols-6 md:grid-rows-2 md:grid-flow-row">
+            <img
+                    class="col-span-1 sm:col-span-2 md:row-span-2 object-cover  w-full h-full aspect-[6/3] rounded-t-lg sm:rounded-tl-lg sm:rounded-tr-none md:rounded-l-lg border-r-1 border-solid border-2 border-gray-200 "
+                    src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2301&q=80"
+                    alt="Conference room"
+                />
+            <div class="col-span-1 sm:col-span-4 md:col-span-4 md:row-span-1 px-4 py-4 sm:px-6 ">
+                <div class="title-and-people flex items-center gap-x-4">
+                    <div class="title flex gap-x-4">
+                        <p class="text-xl font-semibold text-weworkteal-600 truncate">1FA</p>
+                    </div>
+                    <div class="people ml-2 flex-shrink-0 flex gap-x-px">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+                            />
+                        </svg>
+
+                        <p
+                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-weworkteal-100 text-weworkteal-800"
+                        >
+                            3
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mt-2 sm:flex sm:justify-between">
+                    <div class="flex gap-x-4">
+                        <p class="flex items-center text-sm text-gray-500">
+                            <svg
+                                class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11 4a1 1 0 10-2 0v4a1 1 0 102 0V7zm-3 1a1 1 0 10-2 0v3a1 1 0 102 0V8zM8 9a1 1 0 00-2 0v2a1 1 0 102 0V9z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+
+                            Whiteboard
+                        </p>
+                        <p class="flex items-center text-sm text-gray-500">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"
+                                />
+                            </svg>
+
+                            projector
+                        </p>
+                        <p class="flex items-center text-sm text-gray-500">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
+                                />
+                            </svg>
+                            Other amenity
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="container col-span-1  sm:col-span-6 md:col-span-4 md:row-span-1 px-4 py-4 sm:px-6 ">
+                <div class="flex bg-gray-100 h-full rounded-lg relative mx-2">
+                    
+            </div>
+          </div>
+          
+	</a>
+</li>
+
+<style>
+
+
+
+</style>
